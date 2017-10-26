9d9cdbeeddb09f59f3e001523edeb004f21cd495
yingdal <yingdal@tujia.com>
2017-10-26 23:01:02 +0800

    add Project

---
+++ ZhiBoBa/ZhiBoBa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZhiBoBa
+//
+//  Created by 林颖达 on 2017/10/26.
+//  Copyright © 2017年 林颖达. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
